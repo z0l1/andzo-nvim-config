e8ea58724e98140e4b5f2e937d860aea9f1787ad
z0l1 <andzo1998@gmail.com>
2024-01-05 07:43:49 +0100

    telescope(fix) + harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,6 +2,16 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "^-e", function() print("pressed Control E") end)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "^-e", function() print("pressed Control E") end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+
+
+
+function sayHi() 
+	print("HI")
+end
+
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,11 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gp', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.ngrep_string({ search = vim.fn.input("TS Grep > ") });
+	local asd = vim.fn.input("Telescope Grep > ")
+	-- print(asd)
+	-- builtin.ngrep_string({ search = vim.fn.input("TS Grep > ") });
+	-- builtin.ngrep_string({ search = "packer" });
+	-- grep string, hogy a faszba lett ngrep_string
+	builtin.grep_string({ search = asd })
 end)
 
